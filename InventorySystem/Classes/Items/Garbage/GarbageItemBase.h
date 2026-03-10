9137aa9430276c939ee6d989d28a2dfcb53283d8
Deviunter <74955772+deviunter@users.noreply.github.com>
2026-03-10 03:25:37 +0300

    v. 1.0.1

--- InventorySystem/Classes/Items/Garbage/GarbageItemBase.h
+++ InventorySystem/Classes/Items/Garbage/GarbageItemBase.h
@@ -2,6 +2,10 @@
 // Unreal Engine and its logo are trademarks or registered trademarks of Epic Games, Inc.
 // in the United States and elsewhere.All other trademarks are the property of their respective owners.
 
+// Reflection Engine Copyrignt 2026 SPYTHOONA INTERACTIVE.All Rights Reserved.
+// This software and its source code are the intellectual property of SPYTHOONA INTERACTIVE.
+// Unauthorized copying, modification, distribution, or use is strictly prohibited.
+
 #pragma once
 
 #include "CoreMinimal.h"
